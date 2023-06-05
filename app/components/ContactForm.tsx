9a8d05acbc4b0b543186b9e1941812120b7d7424
James Saunders-Wyndham <kyotojim2@gmail.com>
2023-06-06 01:00:26 +0900

    Complete contact page functionality

--- app/components/ContactForm.tsx
+++ app/components/ContactForm.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { FormEvent, useState } from "react";
+import React, { useRef } from "react";
 
 // Form Values
 const initState = {
@@ -12,32 +12,34 @@ const initState = {
 };
 
 export default function ContactForm() {
-  async function handleSubmit(e: any) {
+  const firstNameRef = useRef<HTMLInputElement>(null);
+  const lastNameRef = useRef<HTMLInputElement>(null);
+  const emailRef = useRef<HTMLInputElement>(null);
+  const subjectRef = useRef<HTMLInputElement>(null);
+  const messageRef = useRef<HTMLTextAreaElement>(null);
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const data = {
-      firstName: String(e.target.firstName.value),
-      lastName: String(e.target.lastName.value),
-      email: String(e.target.email.value),
-      subject: String(e.target.subject.value),
-      message: String(e.target.message.value),
+
+    let data = {
+      firstName: firstNameRef.current?.value,
+      lastName: lastNameRef.current?.value,
+      email: emailRef.current?.value,
+      subject: subjectRef.current?.value,
+      message: messageRef.current?.value,
     };
 
-    const response = await fetch("/api/contact", {
+    await fetch("api/contact", {
       method: "POST",
       headers: {
+        Accept: "application/json, text/plain",
         "Content-Type": "application/json",
       },
       body: JSON.stringify(data),
+    }).then((res) => {
+      if (res.status === 200) console.log("Success!!!");
     });
-
-    if (response.ok) {
-      console.log("Message sent successfully.");
-    }
-
-    if (!response.ok) {
-      console.log("Error. Message not sent.");
-    }
-  }
+  };
 
   return (
     <>
@@ -48,19 +50,20 @@ export default function ContactForm() {
           <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
             <label
               className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-              htmlFor="grid-first-name"
+              htmlFor="firstName"
             >
               First Name
             </label>
             <input
               className="appearance-none block w-full bg-slate-100 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-              id="grid-first-name"
+              id="firstName"
               name="firstName"
               type="text"
               placeholder="First Name"
               required
               minLength={2}
               maxLength={20}
+              ref={firstNameRef}
             />
           </div>
 
@@ -68,19 +71,20 @@ export default function ContactForm() {
           <div className="w-full md:w-1/2 px-3">
             <label
               className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-              htmlFor="grid-last-name"
+              htmlFor="lastName"
             >
               Last Name
             </label>
             <input
               className="appearance-none block w-full bg-slate-100 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-blue-200"
-              id="grid-last-name"
+              id="lastName"
               type="text"
               name="lastName"
               placeholder="Last Name"
               required
               minLength={2}
               maxLength={20}
+              ref={lastNameRef}
             />
           </div>
         </div>
@@ -90,17 +94,18 @@ export default function ContactForm() {
           <div className="w-full px-3">
             <label
               className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-              htmlFor="grid-email"
+              htmlFor="email"
             >
               Email Address
             </label>
             <input
               className="appearance-none block w-full bg-slate-100 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-blue-200"
-              id="grid-email"
+              id="email"
               name="email"
               type="email"
               placeholder="********@*****.**"
               required
+              ref={emailRef}
             />
           </div>
         </div>
@@ -109,19 +114,20 @@ export default function ContactForm() {
         <div className="w-full md:w-1/2 mb-6 md:mb-0">
           <label
             className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-            htmlFor="contact-subject"
+            htmlFor="subject"
           >
             Subject
           </label>
           <input
             className="appearance-none block w-full bg-slate-100 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-blue-200"
-            id="contact-subject"
+            id="subject"
             name="subject"
             type="text"
             placeholder="Email Subject"
             required
             minLength={2}
             maxLength={50}
+            ref={subjectRef}
           />
         </div>
 
@@ -141,6 +147,7 @@ export default function ContactForm() {
               id="grid-message"
               required
               name="message"
+              ref={messageRef}
             ></textarea>
           </div>
 
