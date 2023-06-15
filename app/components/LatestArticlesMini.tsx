f7f793ea6b26d115f0fac35b412dbd38a6da64b6
James Saunders-Wyndham <kyotojim2@gmail.com>
2023-06-15 15:22:38 +0900

    Add responsive behaviour to sidebar on article page

--- app/components/LatestArticlesMini.tsx
+++ app/components/LatestArticlesMini.tsx
@@ -3,17 +3,13 @@ import { getLatestPostMini } from "@/sanity/sanity-utils";
 import { urlFor } from "@/lib/urlFor";
 import Link from "next/link";
 
-
 export default async function BlogSmallCard() {
   const posts = getLatestPostMini();
-
- 
 
   return (await posts).map(async (post: any) => {
     return (
       <Link key={post._id} href={`/posts/${post.slug}`}>
-        
-        <div className="card rounded-none w-fit h-fit mx-2 my-4 hover:shadow-lg hover:shadow-slate-200 hover:drop-shadow hover:transition-all duration-300 group">
+        <div className="card rounded-none w-fit h-fit mx-2 my-1 hover:shadow-lg hover:shadow-slate-200 hover:drop-shadow  hover:bg-base-100 hover:transition-all duration-300 group">
           <figure className="">
             <Image
               src={(await urlFor(post.image)).url()}
@@ -25,17 +21,17 @@ export default async function BlogSmallCard() {
             />
           </figure>
           <div className="card-body p-4 my-2 h-fit">
-            <div className="flex items-center h-24 pl-3 pr-2 py-2 border-l-2 border-red-600 xl:h-28">
+            <div className="flex items-center h-24 pl-2 py-2 border-l-2 border-red-600 xl:h-28">
               <h2 className="font-roboto_condensed text-red-900 text-2xl font-bold">
                 {post.name}
               </h2>
             </div>
 
             {/* CATEGORIES & TAGS */}
-            <div className="flex flex-col pt-2 my-3">
+            {/* <div className="flex flex-col pt-2 my-3">
               <div>
                 <div className="flex flex-row justify-start align-middle w-fit h-8 border-t-2 border-b-2 border-white">
-                  {posts.category.map((category) => (
+                  {(await posts).category.map((category) => (
                     <div
                       key={category._id}
                       className="mx-3 px-3 pt-1 font-cardHeading text-red-700 text-md"
@@ -47,10 +43,10 @@ export default async function BlogSmallCard() {
                   ))}
                 </div>
               </div>
-            </div>
+            </div> */}
           </div>
         </div>
-        <div className="divider"></div>
+        <div className="divider mb-1"></div>
       </Link>
     );
   });
--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -86,11 +86,9 @@ export default async function postArticle({ params: { slug } }: Props) {
 
   const post: Post = await createClient(clientConfig).fetch(query, { slug });
 
-  
-
   return (
     <>
-      <main key={post._id} className="flex justify-center">
+      <main key={post._id} className="flex flex-col items-center justify-center xl:items-start xl:flex-row">
         <section className="md:w-[85%] lg:w-[65%] xl:w-[40%] 2xl:w-[35%]flex flex-col justify-center">
           {/* TOP BOARDER, TITLE, AUTHOR */}
           <article className="flex flex-col items-center justify-center">
@@ -192,10 +190,14 @@ export default async function postArticle({ params: { slug } }: Props) {
         </section>
 
         {/* SIDE MENU LATEST ARTICLES */}
-        <section className="w-[18%] mt-80 bg-base-100 border-l-2 border-r-2 border-slate-800">
+        <section className="md:w-[85%] xl:w-[20%] mt-4 xl:mt-80  xl:border-l-4 xl:border-r-4 border-white">
           <article className="flex flex-col px-4">
-            <div><h3 className="text-3xl font-bold">Latest Posts</h3></div>
-<div className="my-4"><LatestArticlesMini /></div>
+            <div className="ml-6">
+              <h3 className="text-4xl font-bold">Latest Posts</h3>
+            </div>
+            <div className="grid grid-cols-2 md:grid-cols-3 gap-4 xl:flex xl:flex-col my-4">
+              <LatestArticlesMini />
+            </div>
           </article>
         </section>
       </main>
