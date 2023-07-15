48639cf7b52fb48002cee10694656c7cdf72f45f
James Saunders-Wyndham <kyotojim2@gmail.com>
2023-07-15 12:31:02 +0900

    Include robots.ts file

---
+++ app/robots.ts
@@ -0,0 +1,14 @@
+import { MetadataRoute } from 'next'
+ 
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: 'all',
+      disallow: '/admin/',
+      
+    },
+    
+    sitemap: 'https://www.romancingjapan.com/sitemap.xml',
+  }
+}
