0d21632b5d78d62abe17323237dd66bedf922a72
James Saunders-Wyndham <kyotojim2@gmail.com>
2023-07-28 02:38:21 +0900

    Change h4 style

--- sanity/schemas/index.ts
+++ sanity/schemas/index.ts
@@ -6,9 +6,7 @@ import tag from "./tag";
 import author from "./author";
 import siteSettings from "./siteSettings";
 import userSignup from "./userEmail";
-import youtube from "./youtube";
 
-
-const schemas = [post, category, tag, author, siteSettings, userSignup, youtube];
+const schemas = [post, category, tag, author, siteSettings, userSignup];
 
 export default schemas;
