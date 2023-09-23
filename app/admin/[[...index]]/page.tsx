5bddfc5055cb0307a050577319ca875d06ce36e7
James Saunders-Wyndham <kyotojim2@gmail.com>
2023-09-24 01:55:32 +0900

    Remove unused dependencies

--- app/admin/[[...index]]/page.tsx
+++ app/admin/[[...index]]/page.tsx
@@ -1,6 +1,9 @@
 "use client";
 
 import config from "../../../sanity.config";
+
+//import dynamic from "next/dynamic";
+
 import { NextStudio } from "next-sanity/studio";
 
 const AdminPage = () => {
--- sanity.config.ts
+++ sanity.config.ts
@@ -1,8 +1,10 @@
-import { defineConfig } from "sanity";
+//import { defineConfig } from "sanity";
 import { deskTool } from "sanity/desk";
 import schemas from "./sanity/schemas";
+const { defineConfig } = dynamic(() => import("sanity"));
 import { deskStructure } from "./deskStructure";
 import { visionTool } from "@sanity/vision";
+import dynamic from "next/dynamic";
 
 // This configures the sanity studio, where the model object is stored.
 const config = defineConfig({
