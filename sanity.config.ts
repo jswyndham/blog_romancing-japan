cb04edf41970ca0fa62f9cea5ed61a42a23ec2b4
James Saunders-Wyndham <kyotojim2@gmail.com>
2023-09-24 12:40:42 +0900

    Add npm packages

--- sanity.config.ts
+++ sanity.config.ts
@@ -11,7 +11,6 @@ const config = defineConfig({
   title: "Blog_Japan",
   apiVersion: "2023-04-25",
   basePath: "/admin",
-  useCdn: true,
   plugins: [
     deskTool({
       structure: deskStructure,
