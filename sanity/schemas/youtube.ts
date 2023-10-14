b676eb8a96f4c941424dd5d3c75e275ce497e547
James Saunders-Wyndham <kyotojim2@gmail.com>
2023-10-14 17:50:32 +0900

    Add youtube link to post block content

--- sanity/schemas/youtube.ts
+++ sanity/schemas/youtube.ts
@@ -1,4 +1,5 @@
 import { defineType, defineField } from "sanity";
+import { YouTubePreview } from "../../app/components/YouTubePreview";
 
 export const youtube = defineType({
   name: "youtube",
@@ -11,4 +12,12 @@ export const youtube = defineType({
       title: "YouTube video URL",
     }),
   ],
+  preview: {
+    select: {
+      url: "url",
+    },
+  },
+  components: {
+    preview: YouTubePreview,
+  },
 });
