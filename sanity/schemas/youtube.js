ef4acd72d7825d269e8b4a8f75c8cde1bb805da1
James Saunders-Wyndham <kyotojim2@gmail.com>
2023-10-14 22:06:35 +0900

    Add youtube preview

---
+++ sanity/schemas/youtube.js
@@ -0,0 +1,39 @@
+import getYouTubeID from "get-youtube-id";
+import { defineType, defineField } from "sanity";
+
+const YouTubePreview = ({ value }) => {
+  const id = getYouTubeID(value.url);
+  const url = `https://www.youtube.com/embed/${id}`;
+  if (!id) {
+    return <div>Missing YouTube URL</div>;
+  }
+  return (
+    <iframe
+      width="560"
+      height="315"
+      src={url}
+      title="YouTube video player"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      allowFullScreen
+    />
+  );
+};
+
+export default {
+  name: "youtube",
+  type: "object",
+  title: "YouTube Embed",
+  fields: [
+    {
+      name: "url",
+      type: "url",
+      title: "URL",
+    },
+  ],
+  preview: {
+    select: {
+      url: "url",
+    },
+  },
+  components: { preview: YouTubePreview },
+};
