6b9e6a1edba967a26c0a3d9bf69612b119d726b2
jswyndham <kyotojim2@gmail.com>
2023-10-18 03:00:45 +0900

    config webpack

--- next.config.js
+++ next.config.js
@@ -12,6 +12,22 @@ const nextConfig = {
       },
     ],
   },
+
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve = {
+        ...config.resolve,
+        fallback: {
+          net: false,
+          dns: false,
+          tls: false,
+          fs: false,
+          request: false,
+        },
+      };
+    }
+    return config;
+  },
   
   async redirects() {
     return [
