1c54190420a6856f10930d223c3c3b7fe1a5e0be
jswyndham <kyotojim2@gmail.com>
2023-10-21 10:30:20 +0900

    Update text spacing and reability for mobile use

--- sanity/schemas/index.ts
+++ sanity/schemas/index.ts
@@ -1,14 +1,13 @@
-"use client";
+'use client';
 
-import post from "./post";
-import category from "./category";
-import tag from "./tag";
-import author from "./author";
-import siteSettings from "./siteSettings";
-import userSignup from "./userEmail";
-import table from "./table";
-import youtube from "./youtube";
+import post from './post';
+import category from './category';
+import tag from './tag';
+import author from './author';
+import siteSettings from './siteSettings';
+import userSignup from './userEmail';
+import table from './table';
 
-const schemas = [post, category, tag, author, siteSettings, userSignup, table, youtube];
+const schemas = [post, category, tag, author, siteSettings, userSignup, table];
 
 export default schemas;
