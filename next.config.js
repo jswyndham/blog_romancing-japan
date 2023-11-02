4a61d6a617331ba2962ef1709452a9c1fc53ea73
James Saunders-Wyndham <kyotojim2@gmail.com>
2023-11-02 14:31:14 +0900

    Delete permanent redirect

--- next.config.js
+++ next.config.js
@@ -3,17 +3,6 @@ const nextConfig = {
   reactStrictMode: false,
   trailingSlash: true,
 
-  async redirects() {
-    return [
-      {
-        source: "/posts/the-top-5-historical-temples-in-kyoto/",
-        destination: "/posts/top-5-historical-temples-in-kyoto/",
-        // Matched parameters can be used in the destination
-        permanent: true,
-      },
-    ];
-  },
-
   images: {
     remotePatterns: [
       {
