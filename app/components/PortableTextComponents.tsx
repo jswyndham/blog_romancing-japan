77b0a420ce3e50acd44090526dc19fc3c2ce8714
jswyndham <kyotojim2@gmail.com>
2024-02-21 02:27:29 +0900

    Add to H3 style - add left border for greater emphasis

--- app/components/PortableTextComponents.tsx
+++ app/components/PortableTextComponents.tsx
@@ -47,7 +47,7 @@ function PortableTextComp() {
 				</h2>
 			),
 			h3: ({ children }: any) => (
-				<h3 className="text-2xl text-info py-3 font-bold border-l-2 border-l-red-700 pl-1">
+				<h3 className="text-2xl text-info py-3 font-bold border-l-4 border-l-red-700 pl-2">
 					{children}
 				</h3>
 			),
