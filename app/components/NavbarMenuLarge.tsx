f8d28ffa8ff4bbecdbc76eb775d8865a39f99cdc
James Saunders-Wyndham <kyotojim2@gmail.com>
2024-04-04 10:19:20 +0900

    Update NavbarMenuLarge.tsx
    Update navbar links to h1 tag.

--- app/components/NavbarMenuLarge.tsx
+++ app/components/NavbarMenuLarge.tsx
@@ -6,42 +6,42 @@ function NavbarMenuLarge() {
     <div className="hidden xl:flex smd: flex-col gap-2">
       {/* NAVBAR MENU */}
       <ul className="flex flex-row py-3 lg:space-x-8 xl:space-x-14 2xl:space-x-20 mr-3 mt-6 lg:mr-6 xl:mr-9 2xl:mr-16 font-roboto_condensed text-white text-xl">
-        <li className="nav-menu">
-          <Link href={"/"}>
-            <h4>Home</h4>
-          </Link>
-        </li>
+				<li className="nav-menu">
+					<Link href={'/'}>
+						<h1>Home</h1>
+					</Link>
+				</li>
 
-        <li className="nav-menu">
-          <Link href={"/blog-collection"}>
-            <h4 className="">Blog Collection</h4>
-          </Link>
-        </li>
+				<li className="nav-menu">
+					<Link href={'/blog-collection'}>
+						<h1 className="">Blog Collection</h1>
+					</Link>
+				</li>
 
-        <li className="nav-menu">
-          <Link href={"/categories"}>
-            <h4>Article Categories</h4>
-          </Link>
-        </li>
+				<li className="nav-menu">
+					<Link href={'/categories'}>
+						<h1>Article Categories</h1>
+					</Link>
+				</li>
 
-        <li className="nav-menu">
-          <Link href={"/tags"}>
-            <h4>Article Tags</h4>
-          </Link>
-        </li>
+				<li className="nav-menu">
+					<Link href={'/tags'}>
+						<h1>Article Tags</h1>
+					</Link>
+				</li>
 
-        <li className="nav-menu">
-          <Link href={"/contact"}>
-            <h4>Contact</h4>
-          </Link>
-        </li>
+				<li className="nav-menu">
+					<Link href={'/contact'}>
+						<h1>Contact</h1>
+					</Link>
+				</li>
 
-        <li className="nav-menu">
-          <Link href={"/about"}>
-            <h4>About us</h4>
-          </Link>
-        </li>
-      </ul>
+				<li className="nav-menu">
+					<Link href={'/about'}>
+						<h1>About us</h1>
+					</Link>
+				</li>
+			</ul>
     </div>
   );
 }
