89d9e5d53e84e0cc27f1fdc34f81d5168aefdf1c
James Saunders-Wyndham <kyotojim2@gmail.com>
2024-04-04 10:01:49 +0900

    Update Tags.tsx
    Add canonical and description text to tags page.

--- app/components/Tags.tsx
+++ app/components/Tags.tsx
@@ -1,6 +1,22 @@
 import { getTags } from '@/sanity/sanity-utils';
 import Link from 'next/link';
 
+export const metadata = {
+	title: 'Romancing Japan Article Tags',
+	description:
+		'This page is a collection of all article tags that identified the main themes related to articles published by Romancing Japan.',
+	alternates: {
+		canonical: `https://www.romancing-japan.com/tags/`,
+	},
+	openGraph: {
+		title: 'Romancing Japan Article Tags',
+		description:
+			'This page is a collection of all article tags that identified the main themes related to articles published by Romancing Japan.',
+		type: 'website',
+		siteName: 'Romancing Japan',
+	},
+};
+
 export default async function Tags() {
 	const tags = await getTags();
 
