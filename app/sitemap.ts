089638d460e39d68a62fb2dde29206dc5295cbfc
jswyndham <kyotojim2@gmail.com>
2024-04-15 09:40:52 +0900

    Add static pages to the sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,43 +1,59 @@
-import { getCategories, getPostsArchive, getTags } from "@/sanity/sanity-utils";
+import { getCategories, getPostsArchive, getTags } from '@/sanity/sanity-utils';
 
 export const revalidate = 50; // revalidate every minute
 
 export default async function sitemap() {
-  const baseUrl = "https://www.romancing-japan.com/";
+	const baseUrl = 'https://www.romancing-japan.com/';
 
-  // Get all posts from sanity-utils
-  const posts = await getPostsArchive();
-  const postsUrls =
-    posts?.map((post) => {
-      return {
-        url: `${baseUrl}posts/${post.slug}/`,
-        lastModified: new Date(),
-      };
-    }) ?? [];
+	// ********** Get all posts from sanity-utils
+	const posts = await getPostsArchive();
+	const postsUrls =
+		posts?.map((post) => {
+			return {
+				url: `${baseUrl}posts/${post.slug}/`,
+				lastModified: new Date(),
+			};
+		}) ?? [];
 
-  // Get all categories from sanity-utils
-  const categories = await getCategories();
-  const categoryUrls =
-    categories?.map((categories) => {
-      return {
-        url: `${baseUrl}categories/${categories.slug}/`,
-        lastModified: new Date(),
-      };
-    }) ?? [];
+	// ******** Get all categories from sanity-utils
+	const categories = await getCategories();
+	const categoryUrls =
+		categories?.map((categories) => {
+			return {
+				url: `${baseUrl}categories/${categories.slug}/`,
+				lastModified: new Date(),
+			};
+		}) ?? [];
 
-  const tags = await getTags();
-  const tagUrls =
-    tags?.map((tags) => {
-      return { url: `${baseUrl}tags/${tags.slug}/`, lastModified: new Date() };
-    }) ?? [];
+	// ************ Get tags
+	const tags = await getTags();
+	const tagUrls =
+		tags?.map((tags) => {
+			return {
+				url: `${baseUrl}tags/${tags.slug}/`,
+				lastModified: new Date(),
+			};
+		}) ?? [];
 
-  return [
-    {
-      url: baseUrl,
-      lastModified: new Date(),
-    },
-    ...postsUrls,
-    ...categoryUrls,
-    ...tagUrls,
-  ];
+	// ********** Static Pages
+	const staticPages = [
+		{
+			url: `${baseUrl}`, // Home page
+			lastModified: new Date(),
+		},
+		{
+			url: `${baseUrl}contact/`, // Contact page
+			lastModified: new Date(),
+		},
+		{
+			url: `${baseUrl}about/`, // About Us page
+			lastModified: new Date(),
+		},
+		{
+			url: `${baseUrl}privacy-policy/`, // Privacy Policy page
+			lastModified: new Date(),
+		},
+	];
+
+	return [...staticPages, ...postsUrls, ...categoryUrls, ...tagUrls];
 }
