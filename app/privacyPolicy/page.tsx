4a7567a1f176adf86ae3d7f13073f066187093c7
James Saunders-Wyndham <kyotojim2@gmail.com>
2024-04-26 14:14:55 +0900

    Extend the metatag description for privacy policy

--- app/privacyPolicy/page.tsx
+++ app/privacyPolicy/page.tsx
@@ -4,11 +4,11 @@ import React from "react";
 export const metadata = {
   title: "Romancing Japan Privacy Policy",
   description:
-    "This page provides detailed information about the privacy policies we uphold in relation to our users, detailing how personal data is collected, used, and protected to ensure the confidentiality and security of user information.",
+    "This page provides detailed information about the privacy policies we uphold in relation to our users, detailing how personal data is collected, used, and protected for confidentiality.",
   openGraph: {
     title: "Romancing Japan Privacy Policy",
     description:
-      "This page provides detailed information about the privacy policies we uphold in relation to our users, detailing how personal data is collected, used, and protected to ensure the confidentiality and security of user information.",
+      "This page provides detailed information about the privacy policies we uphold in relation to our users, detailing how personal data is collected, used, and protected for confidentiality.",
     type: "website",
     siteName: "Romancing Japan",
     url: "https://www.romancing-japan.com/",
