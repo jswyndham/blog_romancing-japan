4563c143ebea47068ce8ba3f6b91bd62f8dfa410
jswyndham <kyotojim2@gmail.com>
2024-05-11 14:23:44 +0900

    Fix sitemap format

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,63 +1,63 @@
-import { getCategories, getPostsArchive, getTags } from "@/sanity/sanity-utils";
+import { getCategories, getPostsArchive, getTags } from '@/sanity/sanity-utils';
 
 export const revalidate = 50; // revalidate every minute
 
 export default async function sitemap() {
-  const baseUrl = "https://www.romancing-japan.com/";
+	const baseUrl = 'https://www.romancing-japan.com/';
 
-  // ********** Get all posts from sanity-utils
-  const posts = await getPostsArchive();
-  const postsUrls =
-    posts?.map((post) => {
-      return {
-        url: `${baseUrl}posts/${post.slug}/`,
-        lastModified: new Date(),
-      };
-    }) ?? [];
+	// ********** Get all posts from sanity-utils
+	const posts = await getPostsArchive();
+	const postsUrls =
+		posts?.map((post) => {
+			return {
+				url: `${baseUrl}posts/${post.slug}/`,
+				lastModified: new Date().toISOString(),
+			};
+		}) ?? [];
 
-  // ******** Get all categories from sanity-utils
-  const categories = await getCategories();
-  const categoryUrls =
-    categories?.map((categories) => {
-      return {
-        url: `${baseUrl}categories/${categories.slug}/`,
-        lastModified: new Date(),
-      };
-    }) ?? [];
+	// ******** Get all categories from sanity-utils
+	const categories = await getCategories();
+	const categoryUrls =
+		categories?.map((categories) => {
+			return {
+				url: `${baseUrl}categories/${categories.slug}/`,
+				lastModified: new Date().toISOString(),
+			};
+		}) ?? [];
 
-  // ************ Get tags
-  const tags = await getTags();
-  const tagUrls =
-    tags?.map((tags) => {
-      return {
-        url: `${baseUrl}tags/${tags.slug}/`,
-        lastModified: new Date(),
-      };
-    }) ?? [];
+	// ************ Get tags
+	const tags = await getTags();
+	const tagUrls =
+		tags?.map((tags) => {
+			return {
+				url: `${baseUrl}tags/${tags.slug}/`,
+				lastModified: new Date().toISOString(),
+			};
+		}) ?? [];
 
-  // ********** Static Pages
-  const staticPages = [
-    {
-      url: `${baseUrl}`, // Home page
-      lastModified: new Date(),
-    },
-    {
-      url: `${baseUrl}contact/`, // Contact page
-      lastModified: new Date(),
-    },
-    {
-      url: `${baseUrl}about/`, // About Us page
-      lastModified: new Date(),
-    },
-    {
-      url: `${baseUrl}privacyPolicy/`, // Privacy Policy page
-      lastModified: new Date(),
-    },
-    {
-      url: `${baseUrl}favicon.ico`, // Favicon
-      lastModified: new Date(),
-    },
-  ];
+	// ********** Static Pages
+	const staticPages = [
+		{
+			url: baseUrl, // Home page
+			lastModified: new Date().toISOString(),
+		},
+		{
+			url: `${baseUrl}contact/`, // Contact page
+			lastModified: new Date().toISOString(),
+		},
+		{
+			url: `${baseUrl}about/`, // About Us page
+			lastModified: new Date().toISOString(),
+		},
+		{
+			url: `${baseUrl}privacyPolicy/`, // Privacy Policy page
+			lastModified: new Date().toISOString(),
+		},
+		{
+			url: `${baseUrl}favicon.ico`, // Favicon
+			lastModified: new Date().toISOString(),
+		},
+	];
 
-  return [...staticPages, ...postsUrls, ...categoryUrls, ...tagUrls];
+	return [...staticPages, ...postsUrls, ...categoryUrls, ...tagUrls];
 }
