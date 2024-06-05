d36d54865b1c2575eca9806ee8f75ad58984ebd0
James Saunders-Wyndham <kyotojim2@gmail.com>
2024-06-05 11:04:21 +0900

    Build comment schema

---
+++ sanity/schemas/comment.ts
@@ -0,0 +1,25 @@
+import { defineType } from "sanity";
+
+export const comment = defineType({
+  name: "comment",
+  title: "Comment",
+  type: "document",
+  fields: [
+    {
+      name: "name",
+      title: "Name",
+      type: "string",
+    },
+    {
+      name: "email",
+      title: "Email",
+      type: "string",
+    },
+    {
+      name: "comment",
+      title: "Comment",
+      type: "text",
+    },
+    { name: "post", title: "Post", type: "reference", to: [{ type: "post" }] },
+  ],
+});
--- sanity/schemas/index.ts
+++ sanity/schemas/index.ts
@@ -1,25 +1,27 @@
-'use client';
+"use client";
 
-import post from './post';
-import category from './category';
-import tag from './tag';
-import author from './author';
-import siteSettings from './siteSettings';
-import userSignup from './userEmail';
-import table from './table';
-import youtube from './youtube';
-import blockContent from './blockContent';
+import post from "./post";
+import category from "./category";
+import tag from "./tag";
+import author from "./author";
+import siteSettings from "./siteSettings";
+import userSignup from "./userEmail";
+import table from "./table";
+import youtube from "./youtube";
+import blockContent from "./blockContent";
+import { comment } from "./comment";
 
 const schemas = [
-	post,
-	category,
-	tag,
-	author,
-	siteSettings,
-	userSignup,
-	table,
-	youtube,
-	blockContent,
+  post,
+  category,
+  tag,
+  author,
+  siteSettings,
+  userSignup,
+  table,
+  youtube,
+  blockContent,
+  comment,
 ];
 
 export default schemas;
