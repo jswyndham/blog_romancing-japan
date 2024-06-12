a56532e0f6ed1e5763ba17cc893b2ba663ef085d
jswyndham <kyotojim2@gmail.com>
2024-06-12 21:41:45 +0900

    Fix initials position in the comment box

--- app/components/AddComment.tsx
+++ app/components/AddComment.tsx
@@ -73,6 +73,7 @@ export default function AddComment({ postId }: Props) {
 					</label>
 					<input
 						{...register('email', {
+							required: true,
 							pattern:
 								/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/i,
 						})}
