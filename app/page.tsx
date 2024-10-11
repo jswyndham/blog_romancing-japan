0b549af878083654b30ba4c4ffd3a01cd3a36563
James Saunders-Wyndham <kyotojim2@gmail.com>
2024-10-11 11:56:28 +0900

    Update the JsonLD script

--- app/page.tsx
+++ app/page.tsx
@@ -76,73 +76,82 @@ export default async function Home() {
   };
 
   return (
-    <article className="flex flex-col items-center justify-center overflow-hidden">
-      {/* Homepage pic */}
-      <figure>
-        <Image
-          src="/opengraph-image.jpg"
-          width={3840}
-          height={1368}
-          alt="Mt.Fuji Black and White"
-          priority={true}
-        />
-      </figure>
+    <>
+      {/* Include the JSON-LD script here */}
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify(homePageJsonLd),
+        }}
+      />
+      <article className="flex flex-col items-center justify-center overflow-hidden">
+        {/* Homepage pic */}
+        <figure>
+          <Image
+            src="/opengraph-image.jpg"
+            width={3840}
+            height={1368}
+            alt="Mt.Fuji Black and White"
+            priority={true}
+          />
+        </figure>
 
-      <div className="h-full w-full aspect-auto flex flex-col justify-center align-middle drop-shadow-2xl md:w-[80%] lg:w-[97%] xl:max-w-screen-xl">
-        {/* Latest banner */}
-        <div className="absolute top-2 w-full bg-slate-700 p-4 flex justify-center text-white text-3xl font-bold">
-          <h1 className="flex flex-row">
-            <span className="hidden lg:flex mr-2">Romancing Japan | </span>
-            Latest Articles
-          </h1>
-        </div>
+        <div className="h-full w-full aspect-auto flex flex-col justify-center align-middle drop-shadow-2xl md:w-[80%] lg:w-[97%] xl:max-w-screen-xl">
+          {/* Latest banner */}
+          <div className="absolute top-2 w-full bg-slate-700 p-4 flex justify-center text-white text-3xl font-bold">
+            <h1 className="flex flex-row">
+              <span className="hidden lg:flex mr-2">Romancing Japan | </span>
+              Latest Articles
+            </h1>
+          </div>
 
-        {/* Article post components */}
+          {/* Article post components */}
 
-        <div className="mt-20 flex flex-col items-center justify-center">
-          <div className="h-full w-full aspect-auto md:grid md:grid-rows-5 md:grid-cols-3 md:gap-8 md:mb-8 md:mt-4 lg:grid-rows-3 lg:grid-cols-4">
-            {/* ARTICLE ONE */}
-            <div className="w-full h-full aspect-auto my-3 md:-mt-1 md:mx-0 md:row-span-1 md:col-span-6 lg:row-span-1 lg:col-span-3 lg:mt-0">
-              <ArticleCardOne />
-            </div>
+          <div className="mt-20 flex flex-col items-center justify-center">
+            <div className="h-full w-full aspect-auto md:grid md:grid-rows-5 md:grid-cols-3 md:gap-8 md:mb-8 md:mt-4 lg:grid-rows-3 lg:grid-cols-4">
+              {/* ARTICLE ONE */}
+              <div className="w-full h-full aspect-auto my-3 md:-mt-1 md:mx-0 md:row-span-1 md:col-span-6 lg:row-span-1 lg:col-span-3 lg:mt-0">
+                <ArticleCardOne />
+              </div>
 
-            <div className="hidden lg:flex lg:mt-4 lg:row-span-2 lg:col-span-1">
-              <SignupCard />
-            </div>
+              <div className="hidden lg:flex lg:mt-4 lg:row-span-2 lg:col-span-1">
+                <SignupCard />
+              </div>
 
-            <div className="w-screen my-6 flex items-center justify-center md:h-56 md:col-span-3 md:-ml-20 smd:-ml-24 md:mx-4 md:-mt-1 lg:hidden">
-              <SignupCardLong />
-            </div>
+              <div className="w-screen my-6 flex items-center justify-center md:h-56 md:col-span-3 md:-ml-20 smd:-ml-24 md:mx-4 md:-mt-1 lg:hidden">
+                <SignupCardLong />
+              </div>
 
-            {/* JAMES & AKARI PROFILE @ LG */}
-            <div className="hidden h-full w-full aspect-square lg:flex lg:items-center lg:h-[80%] lg:mt-28 xl:mt-12 xl:h-[90%] xl:border-l-4 xl:border-r-4 border-white lg:row-span-2 lg:col-span-1 lg:pt-5">
-              <AkariAndJamesProfileLg />
-            </div>
+              {/* JAMES & AKARI PROFILE @ LG */}
+              <div className="hidden h-full w-full aspect-square lg:flex lg:items-center lg:h-[80%] lg:mt-28 xl:mt-12 xl:h-[90%] xl:border-l-4 xl:border-r-4 border-white lg:row-span-2 lg:col-span-1 lg:pt-5">
+                <AkariAndJamesProfileLg />
+              </div>
 
-            {/* ARTICLE TWO */}
-            <div className="md:-mt-64 smd:-mt-60 md:row-span-1 md:col-span-6 lg:mt-0 lg:row-span-1 lg:col-span-2">
-              <ArticleCardTwo />
-            </div>
+              {/* ARTICLE TWO */}
+              <div className="md:-mt-64 smd:-mt-60 md:row-span-1 md:col-span-6 lg:mt-0 lg:row-span-1 lg:col-span-2">
+                <ArticleCardTwo />
+              </div>
 
-            {/* ARTICLE THREE */}
-            <div className="md:row-span-1 md:col-span-6 xl:h-[80%] lg:row-span-1 lg:col-span-3 lg:-mt-2 2xl:mt-0">
-              <ArticleCardThree />
-            </div>
+              {/* ARTICLE THREE */}
+              <div className="md:row-span-1 md:col-span-6 xl:h-[80%] lg:row-span-1 lg:col-span-3 lg:-mt-2 2xl:mt-0">
+                <ArticleCardThree />
+              </div>
 
-            {/* JAMES & AKARI PROFILE @ SM */}
-            <div className="md:-mt-12 md:mb-2 lg:hidden border-white md:row-span-1 md:col-span-6 lg:-mt-2">
-              <AkariAndJamesProfileSm />
+              {/* JAMES & AKARI PROFILE @ SM */}
+              <div className="md:-mt-12 md:mb-2 lg:hidden border-white md:row-span-1 md:col-span-6 lg:-mt-2">
+                <AkariAndJamesProfileSm />
+              </div>
             </div>
           </div>
-        </div>
 
-        {/* DIVIDE */}
-        <div className="divider md:-mt-16 lg:-mt-16 2xl:-mt-24"></div>
+          {/* DIVIDE */}
+          <div className="divider md:-mt-16 lg:-mt-16 2xl:-mt-24"></div>
 
-        <div className="flex flex-col space-y-6 mx-4 mb-6 md:mx-0 md:space-y-0 md:grid md:grid-cols-2 md:gap-4 2xl:grid-cols-3">
-          <BlogSmallCard />
+          <div className="flex flex-col space-y-6 mx-4 mb-6 md:mx-0 md:space-y-0 md:grid md:grid-cols-2 md:gap-4 2xl:grid-cols-3">
+            <BlogSmallCard />
+          </div>
         </div>
-      </div>
-    </article>
+      </article>
+    </>
   );
 }
