d2fdf031d453a45fd1185abf95951aa7550ec3a4
James Saunders-Wyndham <kyotojim2@gmail.com>
2024-10-11 11:42:43 +0900

    Add updated head.tsx file

---
+++ app/head.tsx
@@ -0,0 +1,50 @@
+export default function Head() {
+  const homePageJsonLd = {
+    "@context": "https://schema.org",
+    "@type": "WebSite",
+    name: "Romancing Japan",
+    url: "https://www.romancing-japan.com/",
+    potentialAction: {
+      "@type": "SearchAction",
+      target:
+        "https://www.romancing-japan.com/search?query={search_term_string}",
+      "query-input": "required name=search_term_string",
+    },
+    description:
+      "Informative Articles about Japanese travel, lifestyle, and culture. For those who wish to learn more about Japan and its culture.",
+    publisher: {
+      "@type": "Organization",
+      name: "Romancing Japan",
+      logo: {
+        "@type": "ImageObject",
+        url: "https://www.romancing-japan.com/logo.png",
+      },
+    },
+  };
+
+  return (
+    <>
+      {/* Meta tags not covered by the metadata object */}
+      <meta
+        name="facebook-domain-verification"
+        content="j3hoqq9m2dclmb7l7yvtcu08ptvlsn"
+      />
+      <meta name="google-adsense-account" content="ca-pub-1847015508086202" />
+
+      {/* Google AdSense script */}
+      <script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1847015508086202"
+        crossOrigin="anonymous"
+      ></script>
+
+      {/* JSON-LD structured data */}
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify(homePageJsonLd),
+        }}
+      />
+    </>
+  );
+}
