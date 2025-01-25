afe24e9c52c0ea6fbcffc9e382e5ec45db73e632
jswyndham <kyotojim2@gmail.com>
2025-01-25 21:54:21 +0900

    Add Google Tag Manager-4

--- app/layout.tsx
+++ app/layout.tsx
@@ -144,11 +144,10 @@ export default function RootLayout({
 				{/* Google Tag Manager */}
 				<script>
 					{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                    'https://www.googletagmanager.com/gtm.js?id=GTM-T4C8LM3W';
-                    f.parentNode.insertBefore(j,f);
-                    })(window,document,'script','dataLayer','GTM-T4C8LM3W');`}
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KCFF5T82');`}
 				</script>
 			</head>
 			<body className="bg-base-100 w-screen overflow-x-hidden mx-auto">
