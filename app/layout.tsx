58a9996b1c86ab386a2a8b29ec02a5d81e01747b
jswyndham <kyotojim2@gmail.com>
2025-01-25 22:20:30 +0900

    Add Google Tag Manager-8

--- app/layout.tsx
+++ app/layout.tsx
@@ -157,7 +157,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 						src="https://www.googletagmanager.com/ns.html?id=GTM-KCFF5T82"
 						height="0"
 						width="0"
-						style="display:none;visibility:hidden"
+						className="gtm-iframe"
 					></iframe>
 				</noscript>
 				<GoogleAnalytics
